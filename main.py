6c9bbd2047bca43aa6d6fcfd8a02862e8545a577
rafa <rafaelnaranjo495@gmail.com>
2025-12-28 14:48:54 -0600

    Actualiza el README y mejora la lógica del juego BODMAS; agrega manejo de puntuaciones y generación de preguntas.

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-def main():
-    print("Hello from proyect-bodmas!")
+import my_python_functions as m
+try:
+    user_name = input("Hola, ¿Cuál deseas que sea tu nombre de usuario? ")
+    user_score = int(m.getUserPoint(user_name))
+    if user_score == -1:
+        new_user = True
+        user_score = 0
+    else:
+        new_user = False
 
+    user_choise = 0
 
-if __name__ == "__main__":
-    main()
+    while user_choise !="-1":
+        user_score += m.generateQuestion()
+        election = input("Si no deseas continuar, escribe t, de lo contrario presiona enter ")
+        if election == "t":
+            user_choise = "-1"
+            print("Hasta luego "+user_name)
+    m.updateUsersPoints(new_user, user_name, user_score)
+        
+except Exception as e:
+    print("Ocurrió el siguiente error: "+ e)
--- my_python_functions.py
+++ my_python_functions.py
@@ -21,7 +21,7 @@ def getUserPoint(user_name):
 
 def updateUsersPoints(new_user, user_name, score):
     try:
-        register = "\n"+user_name + "," + score
+        register = "\n"+user_name + "," + str(score)
         if new_user == True:
             file_score = open(file_name, 'a')
             file_score.write(register)
@@ -32,17 +32,66 @@ def updateUsersPoints(new_user, user_name, score):
             for line in file_score:
                 separeted = line.split(',')
                 if separeted[0] == user_name:
-                    register = user_name + "," + score + "\n"
+                    register = user_name + "," + str(score) + "\n"
                     temp_file.write(register)
                 else:
                     temp_file.write(line)
-        temp_file.close()
-        file_score.close()
-        os.remove(file_name)
-        os.rename(temporary_file_name, file_name) 
-        return("Score actualizado correctamente")
+            temp_file.close()
+            file_score.close()
+            os.remove(file_name)
+            os.rename(temporary_file_name, file_name) 
+        #return("Score actualizado correctamente")
     except Exception as e:
-        print("Error ", e)
+        print("Error al actualizar score", e)
+
+def generateQuestion():
+    operand_list = []
+    operator_list = []
+    operator_dic ={1:"+", 2:"-", 3:"**", 4:"*"}
+    question_string = ""
+
+    #Fill the operand_list array
+    for i in range(0,5):
+        operand_list.append(r.randint(1,9))
+
+    #Fill the operator_list array
+    for i in range(0,4):
+        operator = operator_dic[r.randint(1,4)]
+        if len(operator_list)!=0 and operator == "**":
+            if operator_list[-1] == "**":
+                operator = operator_dic[r.randint(1,4)]
+        operator_list.append(operator)
+
+    #Concatenate the "operator" and the "operand" arrays
+    for i in range(len(operator_list)):
+        question_string += str(operand_list[i]) + operator_list[i]
+    '''
+        Add the last element of the array because the iteratation before is for the 4 operators and 4 numbers 
+        but we need the 5 numbers and we need to add the last number at the end of the string
+
+    '''
+    question_string += str(operand_list[-1])
+    '''
+        eval is for "transform" the string expression and evaluated as a mathematical expression. 
+        Gives the result of the expression (e.g: eval(2+3+2) returns 7)
+    '''
+    result = eval(question_string)
+    question_string = question_string.replace("**", "^")
+
+    #Interaction with the user
+    user_answer = input("¿Cuál es el resultado de la siguiente operación?: "+question_string+" ")
+    while True:
+        try:
+            
+            if int(user_answer) == result:
+                print("Correcto, tienes un punto")
+                return 1
+            else:
+                print("Casi, pero te faltó un poco :(, intenta de nuevo :D, la respuesta correcta es: "+str(result))
+                return 0
+        except Exception as e:
+            print("Ocurrió el siguiente error: ",e)
+            user_answer = input("¿Cuál es el resultado de la siguiente operación?: "+ question_string+" ")
 
 if __name__ == "__main__":
-    print(updateUsersPoints(False, "Carol", '300'))
+    generateQuestion()
